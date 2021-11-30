63bd51c939d70293680bb5cbd765ae3f8f80bf84
Nikolay <ShlykovNikolaj@yandex.ru>
2021-11-30 13:35:46 +0300

    Started fixing movments function

--- physics.py
+++ physics.py
@@ -1,1 +1,32 @@
-import math
+import pygame
+
+g = 10  #Ускорение свободного падения
+
+wind_force = 50
+
+
+def move_objects (body, dt):
+    """
+    Перемещает тело
+    """
+
+    ax = wind_force / body.m
+    body.x += ( body.Vx * dt + ax * dt**2 )
+    body.Vx += ax*dt
+
+    body.y += ( body.Vy * dt + g * dt**2 )
+    body.Vy += g * dt
+
+
+
+def recalculate_objects_positions(objects, dt):
+    """Пересчитывает координаты объектов.
+    **dt** — шаг по времени
+    """
+
+    for body in objects:
+        move_objects (body, dt)
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")
