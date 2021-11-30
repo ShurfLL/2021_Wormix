388958c951bac2d075cdcf938cb4e4fbaa75f37b
ShurfLL <shakirov.tt@phystech.edu>
2021-11-30 13:37:36 +0300

    added new classes

--- objects.py
+++ objects.py
@@ -1,1 +1,44 @@
 import math
+import random
+import pygame
+
+
+class Player():
+	def __init__():
+		self.name = ""
+		self.health = 500
+		self.x = 0
+		self.y = 0
+		self.an = 0
+		self.vx = 0
+		self.vy = 0
+		self.ax = 0
+		self.ay = 0
+		self.orientation
+		self.sprite = 'models/cat'
+		self.weapon
+
+
+
+class AbstractWeapon():
+    def __init__():
+        self.name = ""
+        self.caption = ""
+        self.an = 0
+        self.bullet
+        self.orientation
+        self.sprite
+
+
+class AbstractBullet():
+	def __init__():
+		self.name = ""
+		self.an = 0
+		self.x = 0
+		self.y = 0
+		self.vx = 0
+		self.vy = 0
+		self.ax = 0
+		self.ay = 0
+		self.orientation
+		self.sprite
