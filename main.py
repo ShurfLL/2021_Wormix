c1f1ac517dbad48450409e7eea80baea9cc20b78
ShurfLL <shakirov.tt@phystech.edu>
2021-11-30 12:32:37 +0300

    added some basics

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 import math
+import pygame
+
+
+WIDTH = 800
+HEIGHT = 600
+FPS = 30
+
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+cat = pygame.Surface((400,400))
+cat = pygame.Surface.convert_alpha(cat)
+cat.fill((0,0,0,0))
+cat_image = pygame.image.load('models/cat.png').convert()
+cat.blit(cat_image, (0,0))
+screen.blit(cat, (0,0))
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
--- objects.py
+++ objects.py
@@ -1,1 +1,24 @@
 import math
+import random
+import pygame
+
+
+class Player():
+	def __init__():
+		self.name = ""
+		self.health = 500
+		self.x = 0
+		self.y = 0
+		self.vx = 0
+		self.vy = 0
+		self.ax = 0
+		self.ay = 0
+		self.sprite = 'mode'
+
+
+
+class AbstractWeapon():
+	def __init__(self):
+
+		self.owner = 0
+		"""носитель оружия""" 
