6bbd953b31e5b1398b951de402194767f707bfac
Shmyrkov <91532990+Shmyrkov@users.noreply.github.com>
2021-12-06 22:52:25 +0300

    with boom!

--- vis.py
+++ vis.py
@@ -1,32 +1,94 @@
-import pygame as pg
+import pygame
 import sys
+from os import path
 
 W = 1400
 H = 660
 
-sc = pg.display.set_mode((W, H))
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+img_dir = path.join(path.dirname(__file__), 'img')
+sc = pygame.display.set_mode((W, H))
 sc.fill((100, 150, 200))
 
+FPS = 60
+
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, center, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = size
+        self.image = explosion_anim[self.size][0]
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.frame = 0
+        self.last_update = pygame.time.get_ticks()
+        self.frame_rate = 10
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > self.frame_rate:
+            self.last_update = now
+            self.frame += 1
+            if self.frame == len(explosion_anim[self.size]):
+                self.kill()
+            else:
+                center = self.rect.center
+                self.image = explosion_anim[self.size][self.frame]
+                self.rect = self.image.get_rect()
+                self.rect.center = center
+
+
+explosion_anim = {}
+explosion_anim['lg'] = []
+
+for i in range(9):
+    filename = 'regularExplosion0{}.png'.format(i)
+    img = pygame.image.load(filename)
+    img.set_colorkey(BLACK)
+    img_lg = pygame.transform.scale(img, (75, 75))
+    explosion_anim['lg'].append(img_lg)
+
+
 def draw_surface(name):
-    surf = pg.image.load(name)
+    surf = pygame.image.load(name)
     rect = surf.get_rect(bottomright=(W, H))
     sc.blit(surf, rect)
-    pg.display.update()
+    pygame.display.update()
 
-def draw_left_worm(x,y):
-    surf1 = pg.image.load('worms_left.png')
-    new_worm = pg.transform.scale(surf1, (60, 60))
+
+def draw_left_worm(x, y):
+    surf1 = pygame.image.load('worms_left.png')
+    new_worm = pygame.transform.scale(surf1, (60, 60))
     new_worm.set_colorkey((255, 255, 255))
     sc.blit(new_worm, (x, y))
-    pg.display.update()
+    pygame.display.update()
 
-draw_surface('3flour22.jpg')
-draw_left_worm(550, 580)
+def create_boom(x,y):
+    global all_sprites
+    expl = Explosion((x, y), 'lg')
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(expl)
+
+def show_boom():
+    global all_sprites
+    all_sprites.update()
+    all_sprites.draw(sc)
+    pygame.display.update()
+
+create_boom(40, 400)
 
 
 while 1:
-    for i in pg.event.get():
-        if i.type == pg.QUIT:
+    draw_surface('3flour22.jpg')
+    draw_left_worm(300, 580)
+    show_boom()
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
             sys.exit()
 
-    pg.time.delay(20)
+    pygame.time.delay(200)
