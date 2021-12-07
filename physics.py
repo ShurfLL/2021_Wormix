2b4385c6d889a1a690b3fb2c7e9b5a6f4fe8f71f
Nikolay <ShlykovNikolaj@yandex.ru>
2021-12-07 13:24:53 +0300

    splited force functions, defined constants wind_direct, updated move_objects function

--- physics.py
+++ physics.py
@@ -2,23 +2,32 @@ import pygame
 
 g = 10  #Ускорение свободного падения
 
-wind_force = 50
-
+wind_force = 10  #Сила ветра
+wind_direct = 1  #Напревление ветра (определяется знаком)
 
 def move_objects(body, dt):
     """
     Перемещает тело
     """
+    body.ax = 0
+    body.ay = 0
 
+    wind_force(body)
+    gravitational_force(body)
 
-    ax = wind_force / body.m
+    body.Vx += body.ax * dt    #Обновляем параметры по оси 0х
     body.x += ( body.Vx * dt + ax * dt**2 )
-    body.Vx += ax*dt
 
-    body.y += ( body.Vy * dt + g * dt**2 )
-    body.Vy += g * dt
+    body.Vy += body.ay * dt  #Обновляем параметры по оси 0у
+    body.y += ( body.Vy * dt + ay * dt**2 )
 
+def wind_force(body):
+    if wind_direct * body.Vx > 0:
+        body.ax += wind_force / body.m
+    body.ax -= wind_force / body.m
 
+def gravitational_force(body):
+    body.ay += g
 
 def recalculate_objects_positions(objects, dt):
     """Пересчитывает координаты объектов.
