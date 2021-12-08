f4747a8b7d566b01334b29b9e81c68d7a53d1c9a
sHiNkO1975 <shinko.iv@gmail.com>
2021-12-08 21:20:38 +0300

    added new class and functions for future menu

--- interface.py
+++ interface.py
@@ -1,64 +1,95 @@
 import pygame
-from objects import Bazooka
-
-FPS = 30
-WIDTH = 800
-HEIGHT = 500
 
 
-GAMESCREEN = pygame.display.set_mode((WIDTH, HEIGHT)) #
-GAMESCREEN.fill((0, 0, 0)) #
-    
+game_running, game_playing = True, False
+START_KEY, BACK_KEY, DOWN_KEY, UP_KEY, ESCAPE_KEY = 0,0,0,0,0
+
 
 def get_object_pic(weapon):
-    return pygame.image.load(weapon.sprite)
-
-
-class Inventory():
-    def __init__(self, weapons):
-        self.inventory = weapons
-        self.x, self.y = WIDTH/3, HEIGHT*4/5
-    
-    
-    def display_inventory(self, display):
-        dx = 0
-        for gun in self.inventory:
-            window = pygame.Surface((70, 50))
-            window.fill((186, 140, 99))
-            window.blit(pygame.transform.scale(get_object_pic(gun), (70,50)),
-                        (0, 0))
-            display.blit(window,  (self.x + dx, self.y))
-            dx += 70
-        
-        
-    def cursor():
-        pass
-    
-    
-pygame.init()
-'''
-проверка
-inv = Inventory([bazooka()])
-inv.display_inventory(GAMESCREEN)
-'''
-
-class Menu():
-    def __init__(self, game):
-        pass
-    
-
-class PauseMenu(Menu):
-    def __init__(self):
+    if weapon != None:
+        return pygame.image.load(weapon.sprite)
+    else:
         pass
 
-pygame.display.update()
-clock = pygame.time.Clock()
-finished = False
+def draw_text(display, text, size, font_name, color, x, y):
+    font = pygame.font.Font(font_name,size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.center = (x,y)
+    display.blit(text_surface,text_rect)
+    
 
-while not finished:
-    clock.tick(FPS)
+def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            finished = True
+            game_running, game_playing = False, False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                START_KEY = True
+            if event.key == pygame.K_BACKSPACE:
+                BACK_KEY = True
+            if event.key == pygame.K_DOWN:
+                DOWN_KEY = True
+            if event.key == pygame.K_UP:
+                UP_KEY = True
+            if event.key == pygame.K_ESCAPE:
+                ESCAPE_KEY = True
 
-pygame.quit()
+class Inventory():
+    def __init__(self, screen, weapons):
+        self.screen = screen
+        self.inventory = weapons
+        WIDTH, HEIGHT = screen.get_size()
+        self.x, self.y = WIDTH/3, HEIGHT*5/6
+    
+    
+    def display(self):
+        dx = 0
+        if self.inventory[0] == None:
+            return
+        else:
+            for gun in self.inventory:
+                window = pygame.Surface((70, 50))
+                window.fill((186, 140, 99))
+                print(type(get_object_pic(gun)))
+                window.blit(pygame.transform.scale(get_object_pic(gun), 
+                                                   (70,50)), (0, 0))
+                self.screen.blit(window,  (self.x + dx, self.y))
+                dx += 70
+
+
+class Button:
+    def __init__(self, screen: pygame.Surface, text, size: tuple, x=40, y=450, color=(255, 255, 255)):
+        '''
+        text - str: name of the picture or text to draw
+        '''
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.r = size
+        self.color = color
+        self.clicked = False
+        self.text = text
+
+        
+    def display_text(self):
+        button = pygame.Surface((self.r[0], self.r[1]))
+        button.fill((255, 0, 0))
+        draw_text(button, self.text, 16, pygame.font.get_default_font(),
+                  self.color, self.r[0]/2, self.r[1]/2)
+        self.screen.blit(button, (self.x, self.y))
+        
+    def display_pic(self):
+        window = pygame.Surface(self.r)
+        window.fill((255,255,255))
+        window.blit(pygame.transform.scale(pygame.image.load(self.text),
+                                           (70,50)), (0, 0))
+        self.screen.blit(window,  (self.x, self.y))
+        
+        
+    def click(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and game_running == True:
+            mouse_coord = (event.pos)
+            if (abs(mouse_coord[0]-self.x) <= self.r[0] and 
+                abs(mouse_coord[1]-self.y) <= self.r[1]):
+                self.clicked = True
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 from objects import *
 from map_editor import *
-#from interface import *
+from interface import *
 from physics import *
 from vis import *
 
 
 cat = Player()
+inv = Inventory(sc, [cat.weapon])
 cat.x=400
 cat.y = 50
 objects=[cat]
@@ -18,6 +19,7 @@ finished = False
 while not finished:
     clock.tick(FPS)
     draw_map(map_image)
+    inv.display()
     cat.check_for_ground(borders)
     cat.falling(borders)
     for event in pygame.event.get():
