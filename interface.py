58ec0fea8e9a6d8711c48b53a5a9e2897d5044cc
sHiNkO1975 <shinko.iv@gmail.com>
2021-12-13 02:12:49 +0300

    added menu system

--- interface.py
+++ interface.py
@@ -1,10 +1,6 @@
 import pygame
 
 
-game_running, game_playing = True, False
-START_KEY, BACK_KEY, DOWN_KEY, UP_KEY, ESCAPE_KEY = 0,0,0,0,0
-
-
 def get_object_pic(weapon):
     if weapon != None:
         return pygame.image.load(weapon.sprite)
@@ -18,22 +14,6 @@ def draw_text(display, text, size, font_name, color, x, y):
     text_rect.center = (x,y)
     display.blit(text_surface,text_rect)
     
-
-def check_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_running, game_playing = False, False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                START_KEY = True
-            if event.key == pygame.K_BACKSPACE:
-                BACK_KEY = True
-            if event.key == pygame.K_DOWN:
-                DOWN_KEY = True
-            if event.key == pygame.K_UP:
-                UP_KEY = True
-            if event.key == pygame.K_ESCAPE:
-                ESCAPE_KEY = True
 
 class Inventory():
     def __init__(self, screen, weapons):
@@ -51,7 +31,6 @@ class Inventory():
             for gun in self.inventory:
                 window = pygame.Surface((70, 50))
                 window.fill((186, 140, 99))
-                print(type(get_object_pic(gun)))
                 window.blit(pygame.transform.scale(get_object_pic(gun), 
                                                    (70,50)), (0, 0))
                 self.screen.blit(window,  (self.x + dx, self.y))
@@ -59,37 +38,129 @@ class Inventory():
 
 
 class Button:
-    def __init__(self, screen: pygame.Surface, text, size: tuple, x=40, y=450, color=(255, 255, 255)):
-        '''
-        text - str: name of the picture or text to draw
-        '''
-        self.screen = screen
-        self.x = x
-        self.y = y
-        self.r = size
-        self.color = color
+    def __init__(self, screen, x, y, image, scale=1):
+        
+        width, height = image.get_width(), image.get_height()
+        self.image = pygame.transform.scale(image, (int(width*scale), int(height*scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
         self.clicked = False
-        self.text = text
+        self.screen = screen
 
         
-    def display_text(self):
-        button = pygame.Surface((self.r[0], self.r[1]))
-        button.fill((255, 0, 0))
-        draw_text(button, self.text, 16, pygame.font.get_default_font(),
-                  self.color, self.r[0]/2, self.r[1]/2)
-        self.screen.blit(button, (self.x, self.y))
-        
-    def display_pic(self):
-        window = pygame.Surface(self.r)
-        window.fill((255,255,255))
-        window.blit(pygame.transform.scale(pygame.image.load(self.text),
-                                           (70,50)), (0, 0))
-        self.screen.blit(window,  (self.x, self.y))
-        
-        
-    def click(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN and game_running == True:
-            mouse_coord = (event.pos)
-            if (abs(mouse_coord[0]-self.x) <= self.r[0] and 
-                abs(mouse_coord[1]-self.y) <= self.r[1]):
+    def display(self):
+        '''
+        отображает кнопку на поверхности
+        '''
+        self.screen.blit(self.image, (self.rect.x, self.rect.y))
+
+
+    def click(self):
+        '''
+        проверяет, была ли кнопка нажата
+
+        '''
+        action = False
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 self.clicked = True
+                action = True
+                
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+        return self.clicked
+    
+    
+def MainMenu(screen):
+    '''
+    Функция отображает главное меню и возвращает данные о нажатых кнопках
+    '''
+    menu_w, menu_h = screen.get_size()
+    menu_img = pygame.transform.scale(pygame.image.load('models/main_menu.jpg').convert_alpha(),
+                                      screen.get_size())
+    screen.blit(menu_img, (0,0))
+    start_img = pygame.image.load('models/start_btn.png').convert_alpha()
+    exit_img = pygame.image.load('models/exit_btn.png').convert_alpha()
+    settings_img = pygame.image.load('models/settings_btn.png').convert_alpha()
+    info_img = pygame.image.load('models/info_btn.png').convert_alpha()
+    
+    start_button = Button(screen, menu_w/7, menu_h*2/3, start_img, 0.15)
+    exit_button = Button(screen, menu_w*7/20, menu_h*5/6, exit_img, 0.15)
+    setting_button = Button(screen, menu_w*3/5, menu_h*2/3, settings_img, 0.15)
+    info_button = Button(screen, 0, 0, info_img, 0.15)
+
+    exit_button.display()
+    start_button.display()
+    setting_button.display()
+    info_button.display()
+    pygame.display.update()
+    return start_button.click(), exit_button.click(), setting_button.click(), info_button.click()
+
+def AboutMenu(screen):
+    '''
+    Отображает страницу об игре
+
+    '''
+    screen.fill((179, 218, 241))
+    screen.blit(pygame.image.load('models/regularExplosion00.png'), (0,0))
+    delta = 50
+    draw_text(screen, 'made by', 50, pygame.font.get_default_font(), (0,0,0), 300, 200)
+    draw_text(screen, 'ShurfLL', 30, pygame.font.get_default_font(), (0,0,0), 300, 200+delta)
+    draw_text(screen, 'Shmyrkov', 30, pygame.font.get_default_font(), (0,0,0), 300, 200+2*delta)
+    draw_text(screen, 'negaskolya', 30, pygame.font.get_default_font(), (0,0,0), 300, 200+3*delta)
+    draw_text(screen, 'sHiNkO1975', 30, pygame.font.get_default_font(), (0,0,0), 300, 200+4*delta)
+    pygame.display.update()
+
+
+def SettingsMenu(screen):
+    '''
+    Отображает страницу с настройками и возвращает 0 или 1
+
+    '''
+    mid_w, mid_h = screen.get_size()[0]/2, screen.get_size()[1]/2
+    offset = 70
+    screen.fill((179, 218, 241))
+    draw_text(screen, 'Settings', 50, pygame.font.get_default_font(), (0,0,0), mid_w-offset, mid_h-offset)
+    draw_text(screen, 'Volume', 50, pygame.font.get_default_font(), (0,0,0), mid_w, mid_h)
+    close_img = pygame.image.load('models/close.png').convert_alpha()
+    close_button = Button(screen, screen.get_width()*4/5, 0, close_img, 0.15)
+    close_button.display()
+    if close_button.click():
+        return False
+    else:
+        return True
+    
+
+def PauseMenu(screen):
+    '''
+    меню паузы
+    '''
+    new_state = [False, False, False]
+    width, height= screen.get_size()
+    dest = (width*5/6, height/6)
+    offset = 150
+    '''offset_x, offset_y, offset = width/5, width/5, 300
+    pause_screen = pygame.Surface((width*3/5, height*3/5))
+    pause_screen.fill((0,0,0))
+    pause_screen.set_alpha(170)
+    '''
+    close_img = pygame.image.load('models/return.png').convert_alpha()
+    exit_img = pygame.image.load('models/back_to_menu.png').convert_alpha()
+    settings_img = pygame.image.load('models/settings_btn2.png').convert_alpha()
+    
+    close_button = Button(screen, dest[0] - offset, dest[1], close_img, 0.3)
+    exit_button = Button(screen, dest[0] - 2*offset, dest[1], exit_img, 0.3)
+    settings_button = Button(screen, dest[0] - 3*offset, dest[1], settings_img, 0.3)
+    
+    close_button.display()
+    exit_button.display()
+    settings_button.display()
+    
+    if close_button.click():
+        new_state[0] = True
+    if exit_button.click():
+        new_state[1] = True
+    if settings_button.click():
+        new_state[2] = True
+    return new_state
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from interface import *
 from physics import *
 from vis import *
 
-
+paused, playing, settings = False, False, False
 cat = Player()
 inv = Inventory(sc, [cat.weapon])
 cat.x=400
@@ -18,15 +18,57 @@ clock = pygame.time.Clock()
 finished = False
 while not finished:
     clock.tick(FPS)
-    draw_map(map_image)
-    inv.display()
-    cat.check_for_ground(borders)
-    cat.falling(borders)
+    
+    
+    if not playing and not paused:
+        state = MainMenu(sc)
+        if state[1]:
+            finished = True
+        if state[2]:
+            settings = True
+        if state[0]:
+            playing = True
+        if state[3]:
+            AboutMenu(sc)
+            
+            
+    if settings:
+        stg = SettingsMenu(sc)
+        if not stg:
+            settings = False
+            
+            
+    if playing:       
+        draw_map(map_image)
+        pause = Button(sc, W*5/6, H/6, pygame.image.load('models/pause_btn.png').convert_alpha(), 0.15)
+        pause.display()
+        if pause.click():
+            paused = True
+        inv.display()
+        cat.check_for_ground(borders)
+        cat.falling(borders)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+            cat.move(event, borders)
+        draw_object(cat)
+        recalculate_objects_positions(objects, dt)
+        
+    if paused:
+        pause_menu = PauseMenu(sc)
+        if pause_menu[0]:
+            paused = False
+        if pause_menu[1]:
+            playing = False
+            paused = False
+        if pause_menu[2]:
+            settings = True
+            playing = False
+            paused = False
+            
+                
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
-        cat.move(event, borders)
-    draw_object(cat)
-    recalculate_objects_positions(objects, dt)
     pygame.display.update()
 pygame.quit()
